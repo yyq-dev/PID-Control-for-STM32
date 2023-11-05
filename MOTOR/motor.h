306f4ed95c67e3973cc70f13279285471027b859
yyq <123719279+yyq-dev@users.noreply.github.com>
2023-11-05 18:15:57 +0800

    Create motor.h

---
+++ MOTOR/motor.h
@@ -0,0 +1,41 @@
+#ifndef __MOTOR_H
+#define __MOTOR_H
+
+#include "stm32f10x.h"
+
+extern int  motor;                      //电机PWM变量
+
+//位带操作,实现51类似的GPIO控制功能
+//具体实现思想,参考<<CM3权威指南>>第五章(87页~92页).
+//IO口操作宏定义
+#define BITBAND(addr, bitnum) ((addr & 0xF0000000)+0x2000000+((addr &0xFFFFF)<<5)+(bitnum<<2)) 
+#define MEM_ADDR(addr)  *((volatile unsigned long  *)(addr)) 
+#define BIT_ADDR(addr, bitnum)   MEM_ADDR(BITBAND(addr, bitnum)) 
+
+//IO口地址映射
+#define GPIOA_ODR_Addr    (GPIOA_BASE+12) //0x4001080C 
+#define GPIOB_ODR_Addr    (GPIOB_BASE+12) //0x40010C0C    
+
+#define GPIOA_IDR_Addr    (GPIOA_BASE+8) //0x40010808 
+#define GPIOB_IDR_Addr    (GPIOB_BASE+8) //0x40010C08 
+
+ 
+//IO口操作,只对单一的IO口!
+//确保n的值小于16!
+#define PAout(n)   BIT_ADDR(GPIOA_ODR_Addr,n)  //输出 
+#define PAin(n)    BIT_ADDR(GPIOA_IDR_Addr,n)  //输入 
+
+#define PBout(n)   BIT_ADDR(GPIOB_ODR_Addr,n)  //输出 
+#define PBin(n)    BIT_ADDR(GPIOB_IDR_Addr,n)  //输入 
+
+#define PWMA   TIM1->CCR1    //PA8
+#define AIN2   PAout(10)
+#define AIN1   PAout(9)
+
+
+void Set_Pwm(int moto);
+int myabs(int a);
+
+
+#endif
+
